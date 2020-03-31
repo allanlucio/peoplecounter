6184986a57c9d6e959fe9b0c9b534f96ec3e2fef
allan <luciocorreia.allan@gmail.com>
2020-03-31 10:16:20 -0300

    Add a row with two buttons

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,26 @@ void main() {
           Text("Pessoas: 0",
               style:
                   TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FlatButton(
+                child: Text(
+                  "+1",
+                  style: TextStyle(fontSize: 40, color: Colors.white),
+                ),
+                onPressed: () => {},
+              ),
+              FlatButton(
+            child: Text(
+              "-1",
+              style: TextStyle(fontSize: 40, color: Colors.white),
+              
+            ),
+            onPressed: ()=>{},
+          ),
+            ],
+          ),
           Text("Pode Entrar",
               style: TextStyle(
                   color: Colors.white,
