ceaf51e2e23dafaf1445616096bc9a5d1bc5c446
allan <luciocorreia.allan@gmail.com>
2020-03-31 10:33:13 -0300

    Add Image to background screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,43 +3,48 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
       title: "Contador de Pessoas",
-      home: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      home: Stack(
         children: <Widget>[
-          Text("Pessoas: 0",
-              style:
-                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
-          Row(
+          Image.asset("images/restaurant.jpg",
+              fit: BoxFit.cover, height: 1000.0),
+          Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Padding(
-                padding: EdgeInsets.all(10.0),
-                child: FlatButton(
-                  child: Text(
-                    "+1",
-                    style: TextStyle(fontSize: 40, color: Colors.white),
+              Text("Pessoas: 0",
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      child: Text(
+                        "+1",
+                        style: TextStyle(fontSize: 40, color: Colors.white),
+                      ),
+                      onPressed: () => {},
+                    ),
                   ),
-                  onPressed: () => {},
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(10.0),
-                child: FlatButton(
-                  child: Text(
-                    "-1",
-                    style: TextStyle(fontSize: 40, color: Colors.white),
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      child: Text(
+                        "-1",
+                        style: TextStyle(fontSize: 40, color: Colors.white),
+                      ),
+                      onPressed: () => {},
+                    ),
                   ),
-                  onPressed: () => {},
-                ),
+                ],
               ),
-              
+              Text("Pode Entrar",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontStyle: FontStyle.italic,
+                      fontSize: 30))
             ],
-          ),
-          Text("Pode Entrar",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontStyle: FontStyle.italic,
-                  fontSize: 30))
+          )
         ],
       )));
 }
